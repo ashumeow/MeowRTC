33f2aae6bc4f7b4e5247d1d392a08bc22a584b61
Aswini S <ashumeow@live.com>
2015-04-02 11:49:19 +0530

    add improvement

--- MeowRTC/browser/stack/python/django/meowrtc-peer/urls.py
+++ MeowRTC/browser/stack/python/django/meowrtc-peer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-	url(r'offer/', 'start_peer.views.offer', name='offer'),
-	url(r'answer/', 'start_peer.views.answer', name='answer'),
-	url(r'candidate/', 'start_peer.views.candidate', name='candidate'),
+	url(r'offer/', 'meowrtc_peer.views.offer', name='offer'),
+	url(r'answer/', 'meowrtc_peer.views.answer', name='answer'),
+	url(r'candidate/', 'meowrtc_peer.views.candidate', name='candidate'),
 )
